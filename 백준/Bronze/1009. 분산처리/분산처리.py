46e8e68caf98cb8cde3c1ba649fef296e02e1f92
Kisoo Han <hks3847@gmail.com>
2025-04-28 18:55:13 +0900

    [Bronze II] Title: 분산처리, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,11 +1,10 @@
-import math
-
 T = int(input())
 
 for i in range(T):
     a, b = map(int, input().split())
-    result = pow(a, b, 10)
-    if (result == 0):
+    result = pow(a, b, 10) 
+
+    if result == 0:
         print(10)
     else:
         print(result)
