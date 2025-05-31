e18955f54a8eb23b5402958692203431650c73b8
Kisoo Han <hks3847@gmail.com>
2025-05-31 15:02:37 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 292 ms, Memory: 54256 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,15 +1,21 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 
 dic_poke = {}
 dic_num = {}
 for i in range(1,N+1):
-    key = input()
+    key = input().strip()
     dic_poke[key] = i
     dic_num[str(i)] = key
 
 for _ in range(M):
-    a = input()
+    a = input().strip()
     if a.isdigit():
         print(dic_num[a])
     else:
         print(dic_poke[a])
+        
+    
+
