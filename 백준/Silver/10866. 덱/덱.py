ea91202aa462f9a296620506d17c4c220dfddcb5
Kisoo Han <hks3847@gmail.com>
2025-06-02 23:50:26 +0900

    [Silver IV] Title: 덱, Time: 72 ms, Memory: 34968 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,26 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+stack = deque()
+for i in range(N):
+    command = input().split()
+
+    if command[0] == 'push_front':
+        stack.appendleft(command[1])
+    elif command[0] == 'push_back':
+        stack.append(command[1])
+    elif command[0] == 'pop_front':
+        print(-1 if not stack else stack.popleft())
+    elif command[0] == 'pop_back':
+        print(-1 if not stack else stack.pop())
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        print(1 if not stack else 0)
+    elif command[0] == 'front':
+        print(-1 if not stack else stack[0])
+    elif command[0] == 'back':
+        print(-1 if not stack else stack[-1])
