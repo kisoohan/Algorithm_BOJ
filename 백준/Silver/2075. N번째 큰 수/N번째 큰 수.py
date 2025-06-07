c4dab0f8ab3d3dfc09d926dbe5a8b987d9b66cfc
Kisoo Han <hks3847@gmail.com>
2025-06-07 23:52:57 +0900

    [Silver III] Title: N번째 큰 수, Time: 820 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+input = sys.stdin.readline 
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    row = list(map(int, input().split()))
+    for num in row:
+        if len(heap) < N:
+            heapq.heappush(heap, num)
+        else:
+            if num > heap[0]:
+                heapq.heappushpop(heap, num)
+print(heap[0])
